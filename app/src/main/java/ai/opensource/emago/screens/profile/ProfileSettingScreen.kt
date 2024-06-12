99fb67a32ec7867a98ff26b480bd9c864e20d012
Huseong Yoon <huseongyoon@gmail.com>
2024-06-13 01:02:57 +0900

    fix : 오류수정

--- app/src/main/java/ai/opensource/emago/screens/profile/ProfileSettingScreen.kt
+++ app/src/main/java/ai/opensource/emago/screens/profile/ProfileSettingScreen.kt
@@ -22,23 +22,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -151,7 +146,7 @@ fun ProfileSettingScreen(
                                 .width(308.dp)
                                 .height(17.dp)
                                 .padding(start = 8.dp, end = 8.dp)
-                                .clickable { navController.navigate("ProfileSet") }
+                                .clickable { navController.navigate("SetNN") }
                         ) {
                             // Child views.
                             Text(
